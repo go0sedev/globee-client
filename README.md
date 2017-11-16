e23fd225e808cdc48cb9d6ad2445ff0c7dedbfe2
Gustav Trenwith <gtrenwith@gmail.com>
2017-11-16 10:32:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# GloBee PHP Client
+This is a lightweight library to integrate your Laravel website with your GloBee account and accept crypto payments on 
+your website.
+
+## Server Requirements
+The following packages need to be installed on the server:
+```bash
+php 7.1
+php7.2-bcmath
+```
+
+## Installation with Composer
+Run the following command in your project to add this package:
+```bash
+composer require gustavtrenwith/globee_client
+```
+Then run `composer update`.
+
+## Register Service Provider and Facade
+
+Register the service providers and Facades in `config/app.php`.
+```php
+GustavTrenwith\GloBeeClient\GloBeeClientServiceProvider::class,
+```
+```php
+'GloBeeClient' => GustavTrenwith\GloBeeClient\GloBeeClientFacade::class,
+```
+
+Now you can use the `GloBeeClient` facade anywhere in your application.
+
+## Environment Setup
+You need to add the following to your .env file. Then you can easily disable the exception emails by changing the 
+variable value to true.
+```
+ECDSA_SIN=
+ECDSA_PRIVATE_KEY_HEX=
+ECDSA_PUBLIC_KEY_COMPRESSED=
+```
+
+## Generate ECDSA Keypair Values
+You need to use the following code to create the ECDSA Keypair Values. Once done, this code can safely be removed.
+```php
+GloBeeClient::GenerateECDSAKeys();
+```
+An associative array will be returned upon success:
+```php
+Array
+(
+  [private_key_hex] => 7a4fbece43963538cb8f9149b094906168d71be36cfb405e6930fddb42da2c7d
+  [private_key_dec] => 55323065337948610870652254548527896513063178460294714145329611159...
+  [public_key] => 043fbbf44c3da3fec12bf7bac254fd176adc3eaed79470932b574d8d60728eb206fb7a...
+  [public_key_compressed] => 033fbbf44c3da3fec12bf7bac254fd176adc3eaed79470932b574d8d607...
+  [public_key_x] => 3fbbf44c3da3fec12bf7bac254fd176adc3eaed79470932b574d8d60728eb206
+  [public_key_y] => fb7ac7ac6959f75a6859a1a8d745db7e825a3c5c826e5b2e4950892b35772313
+)
+```
+
+## Feedback
+For any questions or suggestions, feel free to contact me on `gtrenwith@gmail.com`
---
+++ src/GloBeeClient.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace GustavTrenwith\GloBeeClient;
+
+/**
+ * Class GloBeeClient
+ * @package GustavTrenwith\GloBeeClient
+ * @author Gustav Trenwith <gtrenwith@gmail.com>
+ */
+class GloBeeClient
+{
+    public static function GenerateECDSAKeys()
+    {
+        return (new \Phactor\Key)->GenerateKeypair();
+    }
+
+    /**
+     * Creates an invoice on the GloBee system and returns the response for the user to be redirected to the payment
+     * interstitial
+     *
+     * @param array $data Array containing the required values that will be sent to GloBee
+     * @param boolean $production True if the live GloBee system is to be used, else it will hit https://test.globee.com
+     * @return string
+     */
+    public static function createInvoice($data = [], $production = false)
+    {
+        if ( ! is_array($data)) {
+            return json_encode([
+                'success' => false,
+                'response' => 'The first parameter should be an array'
+            ]);
+        }
+
+        if ( ! isset($data['currency_code'])) {
+            return json_encode([
+                'success' => false,
+                'response' => 'The first parameter should contain the \'currency_code\' key'
+            ]);
+        }
+
+        if ( ! isset($data['order_id'])) {
+            return json_encode([
+                'success' => false,
+                'response' => 'The first parameter should contain the \'order_id\' key. This value should be unique to identify the order'
+            ]);
+        }
+
+        if ( ! isset($data['notification_url'])) {
+            return json_encode([
+                'success' => false,
+                'response' => 'The first parameter should contain the \'notification_url\' key. This URL should handle the payment callback'
+            ]);
+        }
+
+        if ( ! isset($data['redirect_url'])) {
+            return json_encode([
+                'success' => false,
+                'response' => 'The first parameter should contain the \'redirect_url\' key. This URL should be where the user is directed after they have made payment'
+            ]);
+        }
+
+        if ( ! isset($data['guid'])) {
+            $data['guid'] = Guid::createGuid();
+        }
+
+        if ( ! isset($data['description'])) {
+            $data['description'] = '';
+        }
+
+        if ( ! isset($data['notification_email'])) {
+            $data['notification_email'] = '';
+        }
+
+        if ( ! isset($data['passthrough_data'])) {
+            $data['passthrough_data'] = '';
+        }
+
+        if ( ! isset($data['transaction_speed'])) {
+            $data['transaction_speed'] = 'normal';
+        }
+
+        if ( ! isset($data['buyer_id'])) {
+            $data['buyer_id'] = '';
+        }
+
+        if ( ! isset($data['buyer_name'])) {
+            $data['buyer_name'] = '';
+        }
+
+        if ( ! isset($data['buyer_email'])) {
+            $data['buyer_email'] = '';
+        }
+
+        $url = "https://test.globee.com/invoices";
+        if ($production === true) {
+            $url = "https://globee.com/invoices";
+        }
+        $guid = Guid::createGuid();
+
+        $body = [
+            'price' => (float) $data['price'],
+            'currency' => $data['currency_code'],
+            'token' => env('ECDSA_SIN'),
+            'guid' => $data['guid'],
+            'orderId' => $data['order_id'],
+            'itemDesc' => $data['description'],
+            'notificationEmail' => $data['notification_email'],
+            'notificationURL' => $data['notification_url'],
+            'redirectURL' => $data['redirect_url'],
+            'posData' => $data['passthrough_data'],
+            'transactionSpeed' => $data['transaction_speed'],
+            'buyer' => $data['buyer_id'],
+            'name' => $data['buyer_name'],
+            'email' => $data['buyer_email']
+        ];
+        $headers = [
+            'x-identity' => env('ECDSA_PUBLIC_KEY_COMPRESSED'),
+            'x-signature'=> (new Signature)->generate($url . json_encode($body), env('ECDSA_PRIVATE_KEY_HEX')),
+            'x-accept-version' => '2.0.0',
+            'content-type' => 'application/x-www-form-urlencoded',
+        ];
+        $response = GuzzleHelper::post($url, [
+            'headers' => $headers,
+            'form_params' => $body
+        ]);
+        if ( ! $response) {
+            return json_encode([
+                'success' => false,
+                'response' => $response
+            ]);
+        }
+        $response =  $response->read($response->getSize());
+        $response = json_decode($response);
+
+        if ( ! isset($response->data->url)) {
+            return json_encode([
+                'success' => false,
+                'response' => 'No redirect URL returned'
+            ]);
+        }
+        return json_encode([
+            'success' => true,
+            'response' => 'Please redirect the user to the given URL so they can make payment',
+            'redirect_url' => $response->data->url
+        ]);
+    }
+}
